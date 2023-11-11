b0cbdaf0a2d1d7746141f0835401856667d0287d
Ilya-BISEC <ilya.borisov.bisec@gmail.com>
2023-11-11 21:25:12 +0300

    3nano

--- 3/SerializationApp/SerializationApp/SerializationApp.cpp
+++ 3/SerializationApp/SerializationApp/SerializationApp.cpp
@@ -1,33 +1,34 @@
-﻿#pragma comment (lib, "fltk.lib")
-
-#include "Trainyard.h"
+﻿#include "Trainyard.h"
 #include "window/MainWindow.h"
 
+//#include <gui.hpp>
+//#include <gui/widgets/label.hpp>
+//
+//using namespace nana;
 
 int main()
 {
 
-    Fl_Window window(300, 200, "FLTK Hello World");
-    Fl_Button button(100, 80, 100, 40, "Click me");
-    button.callback(btnCallback);
-    window.end();
-    window.show();
-    return Fl::run();
+    /*form myForm;
 
-    /*Train train1("Moscow", "123", "08:00");
+    label lb{ myForm, rectangle{10,10,100,100} };
+    lb.caption("Hello everybody!");
+    myForm.show();*/
+
+    Train train1("Moscow", "123", "08:00");
     Train train2("Saint Petersburg", "456", "10:30");
-    Train train3("Kazan", "789", "07:45");*/
+    Train train3("Kazan", "789", "07:45");
 
 
     /*train1.binarySerialize(train1, "binTrain1");
     train2.binarySerialize(train2, "binTrain2");
     train3.binarySerialize(train3, "binTrain3");*/
 
-   /* train1.xmlSerialize(train1, "xmlTrain1");
+    train1.xmlSerialize(train1, "xmlTrain1");
     train2.xmlSerialize(train1, "xmlTrain2");
     train3.xmlSerialize(train1, "xmlTrain3");
 
-    std::cout << "Binary and xml serialization finished \n\n\n";*/
+    std::cout << "Binary and xml serialization finished \n\n\n";
 
 
 
@@ -53,10 +54,5 @@ int main()
     //station.printTrainByNumber("456");
     //station.printTrainsAfterTime("08:00");
 
-  
-
-    /*int n;
-    std::cin >> n;
-    return 0;*/
     return 0;
 }
--- 3/SerializationApp/SerializationApp/window/MainWindow.h
+++ 3/SerializationApp/SerializationApp/window/MainWindow.h
@@ -1,20 +1,14 @@
 #pragma once
-#include <FL/Fl.H>
-#include <FL/Fl_Window.H>
-#include <FL/Fl_Button.H>
-#include <FL/fl_message.H>
-
 #include "../stdafx.h"
+
+
 
 #ifdef __MAINWINDOW_H__
 #define __MAINWINDOW_H__
 #endif // __MAINWINDOW_H__
 
 
-void btnCallback(Fl_Widget *widget, void *)
-{
-    fl_message("Hello World!");
-}
+
 
 
 
