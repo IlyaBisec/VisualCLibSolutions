eb11408883cb0695dc633b34515ae39868fbce06
Ilya-BISEC <ilya.borisov.bisec@gmail.com>
2023-11-15 20:39:04 +0300

    COM app

---
+++ 4/COMApplication/COM_CLR_Application/ComApplication.cpp
@@ -0,0 +1,32 @@
+// Main file - entry point
+//#include <wx/wx.h>
+
+
+
+//class MyApp : public wxApp {
+//public:
+//    virtual bool OnInit();
+//};
+//
+//class MyFrame : public wxFrame {
+//public:
+//    MyFrame();
+//};
+//
+//IMPLEMENT_APP(MyApp)
+//
+//bool MyApp::OnInit() {
+//    MyFrame *frame = new MyFrame();
+//    frame->Show(true);
+//    return true;
+//}
+//
+//MyFrame::MyFrame() : wxFrame(NULL, wxID_ANY, "Hello world")
+//{
+//
+//}
+
+int main()
+{
+
+}
