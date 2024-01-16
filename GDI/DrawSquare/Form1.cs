1050fc7636215aa948b9c3ff407a428799e9d123
Ilya-BISEC <ilya.borisov.bisec@gmail.com>
2024-01-16 14:43:51 +0300

    DrawSquareApplication

---
+++ GDI/DrawSquare/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DrawSquare
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        Point startPoint;
+
+        float x1 = 20, x2 = 20;
+        int width = 20, height = 20;
+
+        Rectangle rectangle;
+        bool drawing = false;
+        Color color;
+
+        private void MainForm_Paint(object sender, PaintEventArgs e)
+        {
+            var graphics = e.Graphics;
+            graphics.DrawString("Borisov Ilya", new Font("Arial", 14), Brushes.Blue, new Point(10, 10));
+            graphics.DrawRectangle(new Pen(color, 4), new Rectangle());
+            //graphics.DrawRectangle(new Pen(color, 4), x1,x2,width,height);
+            graphics.FillRectangle(new SolidBrush(color), this.rectangle);
+        }
+
+        private void MainForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                this.drawing = true;
+                this.startPoint = new Point(e.X, e.Y); //e.x
+            }
+            else if (e.Button == MouseButtons.Left)
+            {
+                this.drawing = false;
+                ColorDialog colorDialog = new ColorDialog();
+                colorDialog.AllowFullOpen = false;
+
+                if (colorDialog.ShowDialog() == DialogResult.OK)
+                    color = colorDialog.Color;
+            }
+        }
+
+        private void MainForm_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (this.drawing)
+            {
+                this.rectangle = new Rectangle(this.startPoint.X, this.startPoint.Y, e.X - this.startPoint.X, e.Y -
+this.startPoint.Y);
+                
+                Invalidate();
+            }
+        }
+
+        private void MainForm_MouseUp(object sender, MouseEventArgs e)
+        {
+            this.drawing = false;
+        }
+
+        private void MainForm_KeyDown(object sender, KeyEventArgs e)
+        {
+             int movespeed = 5;
+
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    {
+                        this.rectangle.Y -= movespeed;
+                        break;
+                    }
+                case Keys.S:
+                    {
+                        this.rectangle.Y += movespeed;
+                        break;
+                    }
+                case Keys.A:
+                    {
+
+                        this.rectangle.X -= movespeed;
+                        break;
+                    }
+                case Keys.D:
+                    {
+                        this.rectangle.X += movespeed;
+                        break;
+                    }
+                case Keys.X:
+                    {
+                        this.rectangle = new Rectangle(0, 0, 0, 0);
+                        break;
+                    }
+            }
+            Invalidate();
+        }
+
+    }
+}
---
+++ GDI/DrawSquare/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("DrawSquare")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DrawSquare")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("0534b8a2-c9d0-4799-8de3-7aca4a519f84")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
